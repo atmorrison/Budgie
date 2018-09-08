ba59baf46153ddfdc6d40a1cb1e6dc935c6df8f7
Andre Tupara Morrison <andretuparamorrison@gmail.com>
2018-09-08 16:18:44 +1200

    added header and body components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './AppComponents/Header';
+import Body from './AppComponents/Body';
+
+class App extends React.Component {
+
+    render() {
+        return(
+        <div>
+        <Header/>
+        <Body/>
+        </div>
+        );
+    }
+
+}
